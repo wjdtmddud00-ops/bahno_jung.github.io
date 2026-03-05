6f9a3c56bf3b8199dc183cfda3b26ee2596eddc5
정승영 <jeongseung-yeong@jeongseung-yeong-ui-MacBookPro.local>
2026-03-05 15:27:11 +0900

    chore: collections 동기화

--- 동기화-및-로컬서버.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-echo.
-echo [테스트 서버] 컬렉션 동기화 후 서버 실행...
-echo.
-node run-test-server.js
-echo.
-pause
--- 동기화-및-푸시.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-echo.
-echo [푸시] 컬렉션 동기화 후 Git 푸시...
-echo.
-node sync-and-push.js
-echo.
-pause
